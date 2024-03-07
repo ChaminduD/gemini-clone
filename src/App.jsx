04ed4b08b29bfd9f00dc6e5e6b68b8b87e3140b1
ChaminduD <chamindudulanjana10@gmail.com>
2024-03-07 13:27:26 +0530

    Add css to Main and complete UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Main from './Components/Main/Main.jsx'
 import Sidebar from './Components/Sidebar/Sidebar.jsx'
 
 function App() {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }
